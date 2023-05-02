17c62aa3453c31a38bcfdff0634eeacd8ca0a342
arkulowski <arkulowski1@quinnipiac.edu>
2023-05-01 20:56:04 -0400

    ignore and readme files added

--- gitHelper.java
+++ gitHelper.java
@@ -1,6 +1,7 @@
 package GitHelper;
 
 import java.util.Scanner;
+import java.io.File;
 import git.tools.client.GitSubprocessClient;
 
 public class gitHelper {
@@ -10,6 +11,9 @@ public class gitHelper {
 
         System.out.println("Enter the path of project");
         String repoPath = scanner.nextLine();
+
+        File gitIgnore = new File(repoPath,"gitIgnore");
+        File readMe = new File(repoPath, "README.md");
 
         GitSubprocessClient gitSubprocessClient = new GitSubprocessClient(repoPath);
         gitSubprocessClient.gitInit();
